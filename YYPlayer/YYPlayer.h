9f914506485a4640473dd4a91f0a6130e6643750
yang <yang92139@163.com>
2015-09-02 11:24:15 +0800

    first commit

---
+++ YYPlayer/YYPlayer.h
@@ -0,0 +1,19 @@
+//
+//  YYPlayer.h
+//  YYPlayer
+//
+//  Created by 杨洋 on 15/8/24.
+//  Copyright (c) 2015年 杨洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YYPlayer.
+FOUNDATION_EXPORT double YYPlayerVersionNumber;
+
+//! Project version string for YYPlayer.
+FOUNDATION_EXPORT const unsigned char YYPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YYPlayer/PublicHeader.h>
+
+
